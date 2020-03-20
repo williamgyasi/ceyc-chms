cf8886cf2f12a7fccddd50bb976c1a977b24f423
Eyram Amedzor <amedzor13@gmail.com>
2020-03-20 17:10:50 +0000

    page logo

--- resources/views/pages/payments/create.blade.php
+++ resources/views/pages/payments/create.blade.php
@@ -43,7 +43,7 @@
         @endif
 
         <div class="row justify-content-center mt-5">
-            <div class="row justify-content-center">
+            <div class="justify-content-center">
             <img src="http://ceycairportcity.org/wp-content/uploads/2020/03/Untitled-1.png"
             
             <div class="col-md-6 col-sm-12">
