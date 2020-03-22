d268068f159e931989d5f1b270d6cb64fe8bb07e
Eyram Amedzor <amedzor13@gmail.com>
2020-03-22 08:34:53 +0000

    voucher fx

--- resources/views/pages/givings/create.blade.php
+++ resources/views/pages/givings/create.blade.php
@@ -89,10 +89,6 @@
                                     <option value="Vow">Vow</option>
                                     <option value="Other">Other</option>
                                 </select>
-                                <div class="form-group" id="otherFieldDiv">
-                                    <label for="voucher">Voucher Code</label>
-                                <input type="number" name="voucher" class="form-control" required>
-                                </div>
                             </div>
                             <button type="submit">CONFIRM</button>
                         </form>
@@ -105,35 +101,3 @@
 
 </html>
 
-<script>
-$("#seeAnotherField").change(function() {
-  if ($(this).val() == "yes") {
-    $('#otherFieldDiv').show();
-    $('#otherField').attr('required', '');
-    $('#otherField').attr('data-error', 'This field is required.');
-  } else {
-    $('#otherFieldDiv').hide();
-    $('#otherField').removeAttr('required');
-    $('#otherField').removeAttr('data-error');
-  }
-});
-$("#seeAnotherField").trigger("change");
-
-$("#seeAnotherFieldGroup").change(function() {
-  if ($(this).val() == "yes") {
-    $('#otherFieldGroupDiv').show();
-    $('#otherField1').attr('required', '');
-    $('#otherField1').attr('data-error', 'This field is required.');
-    $('#otherField2').attr('required', '');
-    $('#otherField2').attr('data-error', 'This field is required.');
-  } else {
-    $('#otherFieldGroupDiv').hide();
-    $('#otherField1').removeAttr('required');
-    $('#otherField1').removeAttr('data-error');
-    $('#otherField2').removeAttr('required');
-    $('#otherField2').removeAttr('data-error');
-  }
-});
-$("#seeAnotherFieldGroup").trigger("change");
-
-</script>
