41418e97e3025bedd840664de9ad0cd19775b53b
Eyram Amedzor <amedzor13@gmail.com>
2020-03-22 08:53:17 +0000

    browser suggestion

--- resources/views/pages/givings/confirm.blade.php
+++ resources/views/pages/givings/confirm.blade.php
@@ -77,8 +77,8 @@
                         <h4 class="card-title font-weight-bold">
                             KINDLY CONFIRM THE PAYMENT DETAILS
                         </h4>
-                        <p card-text warning>
-                        Please Complete Your Giving With <a href=https://brave.com target="_blank"> Brave Browser </a> if it Fails
+                        <p card-text warn>
+                        Please Try Giving With <a href=https://brave.com target="_blank"> Another Browser </a> if it This Fails
                         </p>
                         <p class="card-text mt-2">
                             Payment Details
